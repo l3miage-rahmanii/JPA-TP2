1f560a99450fc330dcb0bc668fff388f970e111e
l3miage-rahmanii <imen.rahmanii@univ-grenoble-alpes.fr>
2024-03-14 15:04:24 +0100

    exo2 : ajout des tables choix de strategie d'heritage : single table

---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo2/models/Employe.java
@@ -0,0 +1,26 @@
+package fr.uga.l3miage.tp2.exo2.models;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public abstract class Employe {
+    @Id
+    private Long id;
+
+    private String nom;
+
+    private String email;
+
+    private double salaire;
+
+}
+/*
+* J'ai fais le choix de cette stratégie car elle permet
+* de réduire la complexité grace à la simplicité de la structure
+* de données. De plus il y'a trés peu de differences entre un Manager
+* et un Developpeur.Cette methode permet aussi d'economiser l'espace mémoir
+* occupée. Le seul inconvenient que je trouve dans cette stratégie est
+* la redondance de données que j'ai décidé de ne pas prendre en compte dans
+* mon choix car il n'y'a que 3 champs spécifiques ce qui fait que la redondance seras minime
+* */
---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo2/models/Manager.java
@@ -0,0 +1,10 @@
+package fr.uga.l3miage.tp2.exo2.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Manager extends Employe {
+
+    private String niveauGestion;
+
+}
